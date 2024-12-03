8c07c30d395d04d90b42b115beb071d29cacbd12
halizaarfa <halizansa@gmail.com>
2024-12-03 19:09:42 +0700

    Implemented asynchronously fetching options from JSON endpoint based on dropdown yeay!!

--- lib/spinthewheel/wheel.dart
+++ lib/spinthewheel/wheel.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 import 'fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:setaksetikmobile/explore/models/menu_entry.dart';
 
 class WheelView extends StatefulWidget {
   const WheelView({super.key});
@@ -13,8 +15,10 @@ class WheelView extends StatefulWidget {
 
 class _WheelViewState extends State<WheelView> {
   final StreamController<int> _controller = StreamController<int>();
-  final List<FortuneItem> _wheelItems = [FortuneItem(child: const Text("Start Adding Items!"))];
-  List<String> _menuOptions = [];
+  final List<FortuneItem> _wheelItems = [
+    FortuneItem(child: const Text("Start Adding Items!"))
+  ];
+  List<MenuList> _menuOptions = [];
   String _selectedCategory = "All Categories";
 
   @override
@@ -24,7 +28,17 @@ class _WheelViewState extends State<WheelView> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final request = context.read<CookieRequest>();
+      _fetchMenuOptions(request, _selectedCategory);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Center(
       child: Column(
         children: [
@@ -36,34 +50,45 @@ class _WheelViewState extends State<WheelView> {
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: _spinWheel,
-            child: const Text('Spin'),
             style: ElevatedButton.styleFrom(
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
             ),
+            child: const Text('Spin'),
           ),
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: _clearWheel,
-            child: const Text('Clear'),
             style: ElevatedButton.styleFrom(
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
             ),
+            child: const Text('Clear'),
           ),
           const SizedBox(height: 20),
           DropdownButton<String>(
             value: _selectedCategory,
             items: const [
-              DropdownMenuItem(value: "All Categories", child: Text("All Categories")),
-              DropdownMenuItem(value: "sirloin", child: Text("Sirloin")),
-              DropdownMenuItem(value: "rib Eye", child: Text("Rib Eye")),
-              DropdownMenuItem(value: "lamb", child: Text("Lamb")),
+              DropdownMenuItem(
+                value: "All Categories",
+                child: Text("All Categories"),
+              ),
+              DropdownMenuItem(value: "Beef", child: Text("Beef")),
+              DropdownMenuItem(value: "Chicken", child: Text("Chicken")),
+              DropdownMenuItem(value: "Fish", child: Text("Fish")),
+              DropdownMenuItem(value: "Lamb", child: Text("Lamb")),
+              DropdownMenuItem(value: "Pork", child: Text("Pork")),
+              DropdownMenuItem(value: "Rib Eye", child: Text("Rib Eye")),
+              DropdownMenuItem(value: "Sirloin", child: Text("Sirloin")),
+              DropdownMenuItem(value: "T-Bone", child: Text("T-Bone")),
+              DropdownMenuItem(value: "Tenderloin", child: Text("Tenderloin")),
+              DropdownMenuItem(value: "Wagyu", child: Text("Wagyu")),
+              DropdownMenuItem(value: "Other", child: Text("Other")),
             ],
             onChanged: (String? newValue) {
               if (newValue != null) {
                 setState(() {
                   _selectedCategory = newValue;
                 });
-                _fetchMenuOptions(newValue);
+                _fetchMenuOptions(request, newValue);
               }
             },
             hint: const Text("All Categories"),
@@ -73,59 +98,58 @@ class _WheelViewState extends State<WheelView> {
             height: 200,
             child: SingleChildScrollView(
               child: Column(
-                // children: _menuOptions.map((menu) => buildMenuRow(menu)).toList(),
-                children: [
-                  buildMenuRow("1"),
-                  buildMenuRow("2"),
-                  buildMenuRow("3"),
-                  buildMenuRow("4"),
-                  ]
+                children: _menuOptions.map((menu) {
+                  return buildMenuRow(menu.fields.menu);
+                }).toList(),
               ),
             ),
           ),
-          
         ],
       ),
     );
   }
 
-
-  Future<void> _fetchMenuOptions(String category) async {
-    final url = category == "All Categories"
-        ? 'http://127.0.0.1:8000/option-json/'
-        : 'http://127.0.0.1:8000/option-json/$category/';
+  Future<void> _fetchMenuOptions(CookieRequest request, String category) async {
     try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        setState(() {
-          _menuOptions = data.map((item) => item['fields']['menu'].toString()).toList();
-        });
-      } else {
-        setState(() {
-          _menuOptions = [];
-        });
-      }
-    } catch (e) {
+      final response = await request.get(
+          'http://127.0.0.1:8000/spinthewheel/option-json/$category/');
+      var data = response;
+
       setState(() {
-        _menuOptions = [];
+        _menuOptions.clear();
+        for (var option in data) {
+          if (option != null) {
+            _menuOptions.add(MenuList.fromJson(option));
+          }
+        }
       });
+    } catch (error) {
+      print("Error fetching menu options: $error");
     }
   }
 
   void _addOptionToWheel(String option) {
     setState(() {
       // Remove the placeholder if it's the only item
-      if (_wheelItems.length == 1 && _wheelItems[0].child is Text && (_wheelItems[0].child as Text).data == "Start Adding Items!") {
+      if (_wheelItems.length == 1 &&
+          _wheelItems[0].child is Text &&
+          (_wheelItems[0].child as Text).data == "Start Adding Items!") {
         _wheelItems.clear();
       }
-      _wheelItems.add(FortuneItem(child: Text(option)));
+
+      bool exists = _wheelItems.any((item) => 
+        item.child is Text && (item.child as Text).data == option);
+
+      if (!exists) {
+        _wheelItems.add(FortuneItem(child: Text(option)));
+      }
     });
   }
 
   void _spinWheel() {
-    if (_wheelItems.length != 1) {
-      final int randomIndex = DateTime.now().millisecondsSinceEpoch % _wheelItems.length;
+    if (_wheelItems.length > 1) {
+      final int randomIndex =
+          DateTime.now().millisecondsSinceEpoch % _wheelItems.length;
       _controller.add(randomIndex);
     }
   }
@@ -133,17 +157,11 @@ class _WheelViewState extends State<WheelView> {
   void _clearWheel() {
     setState(() {
       _wheelItems.clear();
-      _wheelItems.add(FortuneItem(child: const Text("Start Adding Items!")));
+      _wheelItems
+          .add(FortuneItem(child: const Text("Start Adding Items!")));
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchMenuOptions(_selectedCategory);
-  }
-
-  
   Widget buildMenuRow(String menuName) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 7.0),
